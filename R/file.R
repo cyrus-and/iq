76ae18a1721da758fbbe0a58673df8b84c0c90a7
Andrea Cardaci <cyrus.and@gmail.com>
2016-07-03 03:13:03 +0200

    Merge `LoadRawFile` into `FromFile`

--- R/file.R
+++ R/file.R
@@ -1,23 +1,3 @@
-#' Load a binary file into a raw vector.
-#'
-#' Accordingly to the file extension, the content of the file is passed to
-#' \code{\link{memDecompress}}.
-#'
-#' @param file.name Input file name.
-#'
-#' @return A raw vector containing the file.
-LoadRawFile <- function(file.name) {
-  # read the whole file
-  file.size <- file.info(file.name)$size
-  raw.samples <- readBin(file.name, what = 'raw', n = file.size)
-  # extract the extension and attempt decompression if needed
-  # XXX gzip never works: internal error -3 in memDecompress(2)
-  extension <- tail(strsplit(file.name, split = '\\.')[[1]], n = 1)
-  compression <- switch(extension, gz = 'gzip', bz2 = 'bzip2', xz = 'xz', 'none')
-  raw.samples <- memDecompress(raw.samples, type = compression)
-  return(raw.samples)
-}
-
 #' Create a signal representation from a file.
 #'
 #' @param file.name Input file name.
@@ -27,7 +7,15 @@ LoadRawFile <- function(file.name) {
 #'
 #' @export
 FromFile <- function(file.name, sample.rate, carrier.frequency = NA) {
-  raw.vector <- LoadRawFile(file.name)
+  # read the whole file
+  file.size <- file.info(file.name)$size
+  raw.vector <- readBin(file.name, what = 'raw', n = file.size)
+  # extract the extension and attempt decompression if needed
+  # XXX gzip never works: internal error -3 in memDecompress(2)
+  extension <- tail(strsplit(file.name, split = '\\.')[[1]], n = 1)
+  compression <- switch(extension, gz = 'gzip', bz2 = 'bzip2', xz = 'xz', 'none')
+  raw.vector <- memDecompress(raw.vector, type = compression)
+  # build the signal from the raw vector
   signal <- FromRawVector(raw.vector, sample.rate, carrier.frequency)
   return(signal)
 }
