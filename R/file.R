49d62085327c81a4f9be8842281d0ffd9dc17330
Andrea Cardaci <cyrus.and@gmail.com>
2016-07-03 03:10:36 +0200

    Add `ToFile` function

--- R/file.R
+++ R/file.R
@@ -1,7 +1,7 @@
 #' Load a binary file into a raw vector.
 #'
 #' Accordingly to the file extension, the content of the file is passed to
-#' \code{memDecompress}.
+#' \code{\link{memDecompress}}.
 #'
 #' @param file.name Input file name.
 #'
@@ -30,4 +30,23 @@ FromFile <- function(file.name, sample.rate, carrier.frequency = NA) {
   raw.vector <- LoadRawFile(file.name)
   signal <- FromRawVector(raw.vector, sample.rate, carrier.frequency)
   return(signal)
+}
+
+#' Write a signal representation to a file.
+#'
+#' The file is compressed according to its extension.
+#'
+#' @param signal Source signal.
+#' @param file.name Output file name.
+#'
+#' @seealso \code{\link{base::memCompress}}
+#'
+#' @export
+ToFile <- function(signal, file.name) {
+  # write to the proper connection according to the file extension
+  extension <- tail(strsplit(file.name, split = '\\.')[[1]], n = 1)
+  con.type <- switch(extension, gz = gzfile, bz2 = bzfile, xz = xzfile, file)
+  con <- con.type(file.name, 'wb')
+  writeBin(signal$raw.vector, con, useBytes = T)
+  close(con)
 }
